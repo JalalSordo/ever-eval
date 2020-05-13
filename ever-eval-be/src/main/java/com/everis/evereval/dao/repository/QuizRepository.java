de11c0549488e0f69e22a1c22eed499caf4b42e1
Abdelkarim Nourti <Kariimnourti15@gmail.com>
2020-05-13 01:01:26 +0000

    ERMT-24 : auto email batch

--- ever-eval-be/src/main/java/com/everis/evereval/dao/repository/QuizRepository.java
+++ ever-eval-be/src/main/java/com/everis/evereval/dao/repository/QuizRepository.java
@@ -5,6 +5,10 @@ import org.springframework.data.repository.CrudRepository;
 import com.everis.evereval.dao.entity.Quiz;
 
 public interface QuizRepository extends CrudRepository<Quiz, Long>{
+	
 	Iterable<Quiz> findAllByDoneAndEvaluated(boolean done,boolean evaluated);
+	
+	Iterable<Quiz> findAllByDoneTrueAndEvaluatedFalse();
+
 
 }
